132d35f5c3fe72fef126b787fe41597e7b106d85
beastboy-net <beastboy9bb99@gmail.com>
2026-06-23 08:10:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig }from 'vite'
 
 export default defineConfig ({
   base: '/maa-mantra-ventures/'
-})
-// https://vite.dev/config/
-export default defineConfig({
   plugins: [react()],
 })
